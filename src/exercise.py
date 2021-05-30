8f3903899f03cbbb31101e7de73ec978069c222e
MrSullivan <>
2021-05-30 19:16:52 +0000

    so so much

--- src/exercise.py
+++ src/exercise.py
@@ -1,8 +1,29 @@
 def main():
     #write your code below this line
     from payment_card import PaymentCard
-    card = PaymentCard(50)
-    print(card)
+    PaulsCard = PaymentCard(20)
+    MattsCard = PaymentCard(30)
+
+    PaulsCard.eat_heartily()
+    MattsCard.eat_affordably()
+
+    print("Paul: " + str(PaulsCard))
+    print("Matt: " + str(MattsCard))
+
+    PaulsCard.add_money(20)
+    MattsCard.eat_heartily()
+
+    print("Paul: " + str(PaulsCard))
+    print("Matt: " + str(MattsCard))
+
+    PaulsCard.eat_affordably()
+    PaulsCard.eat_affordably()
+    MattsCard.add_money(50)
+
+    print("Paul: " + str(PaulsCard))
+    print("Matt: " + str(MattsCard))
+
+
 
 if __name__ == '__main__':
     main()
--- src/payment_card.py
+++ src/payment_card.py
@@ -5,3 +5,27 @@ class PaymentCard:
 
   def __str__(self):
     return str("The card has a balance of " + str(self.opening_balance) + " pounds")
+
+  def eat_affordably(self):
+    if(self.opening_balance - 2.6 >= 0):
+      self.opening_balance = float(self.opening_balance - 2.6)
+    else:
+      return self.opening_balance
+
+
+  def eat_heartily(self):
+    if(self.opening_balance - 4.6 >= 0):
+      self.opening_balance = float(self.opening_balance - 4.6)
+    else:
+      return self.opening_balance
+
+  def add_money(self,amount):
+    self.amount = amount
+    if(self.opening_balance +self.amount <0):
+      return self.opening_balance
+    elif(self.opening_balance + self.amount <= 150):
+      self.opening_balance = self.opening_balance + self.amount
+    else:
+      self.opening_balance = 150
+
+      
